bdda570d6824a4012ed12ead22787c02e880e06c
Paolo Dávila <pdavila@iesebre.com>
2015-03-17 08:44:50 +0100

    Button not function

---
+++ app/src/main/java/org/acacha/ebre_escool/ebre_escool_app/managmentsandbox/employees/EmployeesView.java
@@ -0,0 +1,10 @@
+package org.acacha.ebre_escool.ebre_escool_app.managmentsandbox.employees;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by pdavila on 4/03/15.
+ */
+public class EmployeesView extends Fragment {
+
+}
